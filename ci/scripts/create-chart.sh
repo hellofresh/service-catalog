e9606816c54442afbdeb9c4ff7e54042718587d6
Anton Ustyuzhanin <au@hellofresh.com>
2021-01-06 16:28:42 +0100

    CI: fix create-chart script

--- ci/scripts/create-chart.sh
+++ ci/scripts/create-chart.sh
@@ -17,13 +17,12 @@ echo "Creating package"
 mkdir -p "$CHART_TMP_PATH"
 cp -r "${CHART_PATH}/." "$CHART_TMP_PATH"
 
-helm init --client-only --stable-repo-url https://charts.helm.sh/stable
 helm dependency update "${CHART_TMP_PATH}"
 helm package "${CHART_TMP_PATH}" --version="${VERSION}" -d /tmp
 
 # Push chart to Artifactory
 if [ -n "${ARTIFACTORY_USERNAME}" ] && [ -n "${ARTIFACTORY_PASSWORD}" ]; then
-  user_args=("--user" "${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}")
+  user_args="--user ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}"
 fi
 CHART_PACKAGE="${CHART_NAME}-${VERSION}.tgz"
 CHART_PACKAGE_PATH="/tmp/${CHART_PACKAGE}"
@@ -33,7 +32,7 @@ CHART_PACKAGE_SHA1=$(sha1sum "${CHART_PACKAGE_PATH}" | cut -d ' ' -f 1)
 CHART_PACKAGE_SHA256=$(sha256sum "${CHART_PACKAGE_PATH}" | cut -d ' ' -f 1)
 CHART_PACKAGE_MD5=$(md5sum "${CHART_PACKAGE_PATH}" | cut -d ' ' -f 1)
 
-user_args="--header X-Checksum-MD5:${CHART_PACKAGE_MD5} --header X-Checksum-Sha1:${CHART_PACKAGE_SHA1} --header X-Checksum-Sha256:${CHART_PACKAGE_SHA256}"
+user_args="${user_args} --header X-Checksum-MD5:${CHART_PACKAGE_MD5} --header X-Checksum-Sha1:${CHART_PACKAGE_SHA1} --header X-Checksum-Sha256:${CHART_PACKAGE_SHA256}"
 
 # Upload package
 curl $user_args --fail --upload-file "${CHART_PACKAGE_PATH}" "${CHART_REPO}/${CHART_PACKAGE}"
